0e87430ba6fc10ed6169b1641f38decf43ce46ea
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-04 08:23:10 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,9 +1,9 @@
-chrome.contentSettings.javascript.get({ primaryUrl: "https://gigazine.net" }, e => (
+chrome.contentSettings.javascript.get({ primaryUrl: "https://gigazine.net" }, async e => (
   e.setting == "allow" && chrome.contentSettings.javascript.set({
     primaryPattern: "https://gigazine.net/*",
     setting: "block"
   }),
-  chrome.userScripts.unregister(),
+  await chrome.userScripts.unregister(),
   chrome.userScripts.register([{
     id: "0",
     js: [{ code: "{let g=document.images,i=g.length,p,s;while((s=(p=g[--i]).dataset.src)&&(p.src=s),i);}" }],
