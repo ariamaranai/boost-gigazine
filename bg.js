3f55d2767b007208224b5d62c64a2221614d0037
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-25 21:58:58 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,4 +1,17 @@
-chrome.runtime.onInstalled.addListener(() => (
+chrome.runtime.onStartup.addListener(() => {
+  let { userScripts } = chrome;
+  userScripts &&
+  userScripts.getScripts(scripts =>
+    scripts.length ||
+    userScripts.register([{
+      id: "0",
+      js: [{ code: "for(let g=document.images,i=0,p,s;i<g.length;++i)(s=(p=g[i]).dataset.src)&&(p.src=s)" }],
+      matches: ["https://gigazine.net/", "https://gigazine.net/P*"],
+      runAt: "document_end"
+    }])
+  );
+});
+chrome.runtime.onInstalled.addListener(() =>
   chrome.contentSettings.javascript.get({
     primaryUrl: "https://gigazine.net"
   }, details =>
@@ -7,11 +20,5 @@ chrome.runtime.onInstalled.addListener(() => (
       primaryPattern: "https://gigazine.net/*",
       setting: "block"
     })
-  ),
-  chrome.userScripts.register([{
-    id: "0",
-    js: [{ code: "for(let g=document.images,i=0,p,s;i<g.length;++i)(s=(p=g[i]).dataset.src)&&(p.src=s)" }],
-    matches: ["https://gigazine.net/", "https://gigazine.net/P*"],
-    runAt: "document_end"
-  }])
-));
+  )
+);
