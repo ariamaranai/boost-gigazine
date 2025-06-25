4be89c27a578eb9e1083691adf02036041499d32
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-25 23:44:32 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,25 +1,30 @@
-chrome.runtime.onStartup.addListener(() => {
-  let { userScripts } = chrome;
-  userScripts &&
-  userScripts.getScripts(scripts =>
-    scripts.length ||
-    userScripts.register([{
-      id: "0",
-      js: [{ code: "for(let g=document.images,i=0,p,s;i<g.length;++i)(s=(p=g[i]).dataset.src)&&(p.src=s)" }],
-      matches: ["https://gigazine.net/", "https://gigazine.net/P*"],
-      runAt: "document_end"
-    }])
-  );
-});
-chrome.runtime.onInstalled.addListener(() => (
-  chrome.contentSettings.javascript.get({
-    primaryUrl: "https://gigazine.net"
-  }, details =>
-    details.setting == "allow" &&
-    chrome.contentSettings.javascript.set({
-      primaryPattern: "https://gigazine.net/*",
-      setting: "block"
-    })
-  ),
-  chrome.runtime.onStartup.dispatch()
-));
+{
+  let f = () => {
+    let { userScripts } = chrome;
+    userScripts &&
+    userScripts.getScripts(scripts =>
+      scripts.length || (
+        userScripts.register([{
+          id: "0",
+          js: [{ code: "for(let g=document.images,i=0,p,s;i<g.length;++i)(s=(p=g[i]).dataset.src)&&(p.src=s)" }],
+          matches: ["https://gigazine.net/", "https://gigazine.net/P*"],
+          runAt: "document_end"
+        }]),
+        chrome.runtime.onStartup.removeListener(f)
+      )
+    );
+  }
+  chrome.runtime.onStartup.addListener(f);
+  chrome.runtime.onInstalled.addListener(() => (
+    chrome.contentSettings.javascript.get({
+     primaryUrl: "https://gigazine.net"
+    }, details =>
+      details.setting == "allow" &&
+      chrome.contentSettings.javascript.set({
+        primaryPattern: "https://gigazine.net/*",
+        setting: "block"
+      })
+    ),
+    f()
+  ));
+}
