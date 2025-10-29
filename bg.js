4db60fead797cdfeb0c3ccaeaaa57a8e91942cc1
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-30 02:22:03 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,20 +1,20 @@
 {
-  let f = () => {
+  let onStartup = () => {
     let { userScripts } = chrome;
     userScripts &&
     userScripts.getScripts(scripts =>
       scripts.length || (
         userScripts.register([{
           id: "0",
-          js: [{ code: "for(let g=document.images,i=0,p,s;i<g.length;++i)(s=(p=g[i]).dataset.src)&&(p.src=s)" }],
-          matches: ["https://gigazine.net/", "https://gigazine.net/P*"],
+          js: [{ code: "for(let g=document.images,p,s,i=0;i<g.length;++i)(s=(p=g[i]).dataset.src)&&(p.src=s)" }],
+          matches: ["https://gigazine.net/","https://gigazine.net/P*"],
           runAt: "document_end"
         }]),
-        chrome.runtime.onStartup.removeListener(f)
+        chrome.runtime.onStartup.removeListener(onStartup)
       )
     );
   }
-  chrome.runtime.onStartup.addListener(f);
+  chrome.runtime.onStartup.addListener(onStartup);
   chrome.runtime.onInstalled.addListener(() => (
     chrome.contentSettings.javascript.get({
      primaryUrl: "https://gigazine.net"
@@ -25,6 +25,6 @@
         setting: "block"
       })
     ),
-    f()
+    onStartup()
   ));
 }
